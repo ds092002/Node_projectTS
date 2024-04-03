580823411074491996714c87db563b9b4189b602
Dhaval Solanki <dhavalas24@gmail.com>
2024-04-03 18:35:00 +0530

    03 April

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+import express from 'express';
+const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const port : Number = Number(process.env.PORT);
+const dbURL: string = process.env.MONGO_DB_URL as string;
+
+app.use(express.json());
+
+
+app.listen(port, async () => {
+    mongoose.connect(dbURL)
+    .then(() => console.log('DB is Connected Successfully.....👍🏻'))
+    .catch(error => console.log(error.message));
+    console.log(`Server Start at http://localhost:${port}`);    
+});
