d6827b4b958371b6b22fe43b6d9917bf3bf328ec
Dhaval Solanki <dhavalas24@gmail.com>
2024-05-06 11:36:12 +0530

    06 May

--- src/controller/user/order_controller.ts
+++ src/controller/user/order_controller.ts
@@ -10,28 +10,37 @@ declare global {
         interface Request {
             user?: any;
         }
-    }
-    namespace Express {
-        interface Response {
-            user?: object | undefined;
-        }
-    }    
+    }  
 }
 
-export const addNewOrder = async ( req: Request, res: Response) => {
+export const addNewOrder = async (req: Request, res: Response) => {
     try {
-        let cartItems = await cartServices.getAllCarts( req.query, res.user);
-        console.log(cartItems);
+
+        let cartItems = await cartServices.getAllCarts(req.query, req.user);
+        // console.log(cartItems);
         if (cartItems.length === 0) {
-            return res.status(404).json({ message: `Cart Not Found.....`})
+            return res.status(404).json({ message: `Cart Not Found....`});
         }
-        console.log(cartItems);
-        let orderItems = cartItems.map((item: any) => {
+            //   console.log(cartItems);
+        let orderItems = cartItems.map((item: any) => ({
             product: item.cartItem._id,
             quantity: item.quantity,
             price: item.cartItem.price
-        })
-    } catch (error) {
+        }));
+            console.log(orderItems);
+        let totalPrice = orderItems.reduce((total: number, item: any) => (total + (item.price * item.quantity)), 0);
         
+        let newOrder = await orderServices.addToOrder({
+            user: req.user,
+            items: orderItems,
+            totalAmount: totalPrice
+        });
+        // console.log(newOrder);
+        await cartServices.updateMany(req.user, { isDelete: true });
+        
+        return res.status(201).json({ newOrder, message: `Order Place Successfully` });
+    } catch (error: any) {
+        console.error(error);
+        return res.status(500).json({ message: `Internal Server Error ${error.message}` });
     }
-}
+};
